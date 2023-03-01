7d218d9b7081a2ef64b4fe8e24e7c7b170316d36
PaulaHCA <paulahca53@gmail.com>
2023-03-01 12:53:49 +0100

    Update: rutas hijas

--- src/app/components/usuario/usuario-nuevo.component.ts
+++ src/app/components/usuario/usuario-nuevo.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-usuario-nuevo',
-  template:``
+  template:`
+    <p>Usuario nuevo</p>
+  `
 })
 export class UsuarioNuevoComponent {
 
